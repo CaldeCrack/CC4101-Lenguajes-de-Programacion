63954a2c6c07a55624bce69604bd3c1f32d1b891
Andrés Calderón Guardia <andres.calderon@ug.uchile.cl>
2024-11-07 02:41:29 -0300

    Parte 1 completada

--- Tarea_3/T3.rkt
+++ Tarea_3/T3.rkt
@@ -1,113 +1,153 @@
 #lang play
 
-#|
+;; NOMBRE Y APELLIDO: Andrés Calderón
+;; RUT: 21.273.734-8
+;; Hizo Ud uso de la whiteboard policy: NO
 
-Hizo Ud uso de la whiteboard policy: (Indique SI/NO)
-En caso que afirmativo, indique con quién y sobre qué ejercicio:
--
--
-
-|#
 
 ;;------------------;;
 ;; P1.a, P1.e, P2.a ;;
 ;;------------------;;
 
-
-#|
-<expr> ::= ...
-|#
+;; <expr> ::= (num <Integer>)
+;; 			| (add <expr> <expr>)
+;; 			| (nil)
+;; 			| (conz <expr> <expr>)
+;; 			| (fun <pattern> <expr>)
+;; 			| (app <expr> <expr>)
+;; 			| (id <sym>)
+;; Constructor de expresiones del lenguaje
 (deftype Expr
-  (num n)
-  (add l r)
-  ; ...
-  )
+	(num n)
+	(add l r)
+	(nil)
+	(conz l r)
+	(fun p e)
+	(app f x)
+	(id x)
+)
 
 
 ;;------------------;;
 ;; P1.b, P1.e, P2.b ;;
 ;;------------------;;
 
-
-;; parse : s-expr -> Expr
+;; <s-expr> ::= <Integer>
+;; 				| <sym>
+;; 				| '(nil)
+;; 				| (list '+ <s-expr> <s-expr>)
+;; 				| (list 'cons <s-expr> <s-expr>)
+;; 				| (list 'list (list <s-expr>))
+;; 				| (list 'fun <pattern> <s-expr>)
+;; 				| (list <s-expr> <s-expr>)
+;; parse : <s-expr> -> <Expr>
+;; Parsea el lenguaje funcional
 (define (parse s-expr)
-  (match s-expr
-    [(? number? n) (num n)]
-    [(list '+ l r) (add (parse l) (parse r))]
-    ; ...
-    ))
+	(match s-expr
+		[(? number? n) (num n)]
+		[(? symbol? x) (id x)]
+		['(nil) (nil)]
+		[(list '+ l r) (add (parse l) (parse r))]
+		[(list 'cons l r) (conz (parse l) (parse r))]
+		[(list 'list elems ...) (foldr (λ (elem lista) (conz (parse elem) lista)) (nil) elems)]
+		[(list 'fun p e) (fun (parse-pattern p) (parse e))]
+		[(list f x) (app (parse f) (parse x))]
+	)
+)
 
 
 ;;----- ;;
 ;; P1.c ;;
 ;;----- ;;
 
-#|
-<pattern> ::= ...
-|#
+;; <pattern> ::= (numP <Integer>)
+;; 				| (nilP)
+;; 				| (varP <sym>)
+;; 				| (conzP <pattern> <pattern>)
+;; Constructor de patrones del lenguaje
 (deftype Pattern
-  ; ...
-  )
+	(numP n)
+	(nilP)
+	(varP x)
+	(conzP l r)
+)
+
 
 ;;----- ;;
 ;; P1.d ;;
 ;;----- ;;
 
+;; <s-expr> ::= <Integer>
+;; 				| '(nil)
+;; 				| <sym>
+;; 				| (list 'cons <s-expr> <s-expr>)
+;; 				| (list 'list (list <s-expr>))
 ;; parse-pattern : s-expr -> Pattern
-(define (parse-pattern s-expr) '???)
+;; Parsea los patrones del lenguaje
+(define (parse-pattern s-expr)
+	(match s-expr
+		[(? number? n) (numP n)]
+		['(nil) (nilP)]
+		[(? symbol? x) (varP x)]
+		[(list 'cons l r) (conzP (parse-pattern l) (parse-pattern r))]
+		[(list 'list elems ...) (foldr (λ (elem lista) (conzP (parse-pattern elem) lista)) (nilP) elems)]
+	)
+)
+
 
 ;;----- ;;
 ;; P1.f ;;
 ;;----- ;;
 
-#|
-<value> ::= ...
-|#
+;; <value> ::= (numV <Integer>)
+;; 				| (conzV <value> <value>)
+;; 				| (closureV <sym> <expr> <env>)
+;; Constructor para los valores del lenguaje
 (deftype Value
-  (numV n)
-  ; ...
-  )
+	(numV n)
+	(nilV)
+	(conzV l r)
+	(closureV id body env)
+)
 
 #|
 BEGIN utility definitions
 |#
 
-#|
-<env> ::= (mtEnv)
-       | (extEnv <sym> <value> <env>)
-|#
+;; <env> ::= (mtEnv)
+;; 			| (extEnv <sym> <value> <env>)
+;; Constructor de los ambientes del lenguaje
 (deftype Env
-  (mtEnv)
-  (extEnv x v env))
+	(mtEnv)
+	(extEnv x v env))
 
 ;; extend-env : Symbol Value Env -> Env
 (define (extend-env x v env)
-  (extEnv x v env))
+	(extEnv x v env))
 
 ;; empty-env : Env
 (define empty-env (mtEnv))
 
 ;; extend-env* : (Listof (Symbol * Value)) Env -> Env
 (define (extend-env* bindings env)
-  (foldr
-   (lambda (binding env) (match binding [(cons x v) (extend-env x v env)]))
-   env
-   bindings))
+	(foldr
+		(lambda (binding env) (match binding [(cons x v) (extend-env x v env)]))
+		env
+		bindings))
 
 ;; lookup-env : Symbol Env -> Value
 (define (lookup-env x env)
-  (match env
-    [(mtEnv) (error "LookupError: variable ~a not found" x)]
-    [(extEnv y v env) (if (equal? x y) v (lookup-env x env))]))
+	(match env
+		[(mtEnv) (error "LookupError: variable ~a not found" x)]
+		[(extEnv y v env) (if (equal? x y) v (lookup-env x env))]))
 
 ;; num+ : Value Value -> Value
 (define (num+ v1 v2)
-  (match v1
-    [(numV n) (match v2
-                [(numV m) (numV (+ n m))]
-                [_ (error "TypeError: expected a number")])]
-    [_ (error "TypeError: expected a number")]))
-
+	(match v1
+		[(numV n) (match v2
+				[(numV m) (numV (+ n m))]
+				[_ (error "TypeError: expected a number")])]
+		[_ (error "TypeError: expected a number")]))
 
 #|
 END utility definitions
@@ -118,23 +158,88 @@ END utility definitions
 ;; P1.g ;;
 ;;----- ;;
 
-#|
-<result> e v ::= (failure e)
-               | (success v)
-|#
+;; <result> e v ::= (failure e)
+;; 				| (success v)
+;; Constructor de resultados del lenguaje
 (deftype Result
-  (failure e)
-  (success v))
+	(failure e)
+	(success v))
 
 ;; generate-substs : Pattern Value -> (Result String (Listof (Symbol * Value)))
-(define (generate-substs p v) '???)
+;; A partir de un patrón y un valor, retorna una lista de substituciones o un mensaje de error
+(define (generate-substs p v)
+	;; Función auxiliar de generate-substs
+	(define (aux-generate-substs p v)
+		(match p
+			[(numP x) (match v
+				[(numV y) (if (= x y)
+					'()
+					(failure "MatchError: given number does not match pattern"))]
+				[else (failure "MatchError: expected a number")]
+			)]
+			[(nilP) (match v
+				[(nilV) '()]
+				[else (failure "MatchError: expected nil")]
+			)]
+			[(varP x) (list (cons x v))]
+			[(conzP x y) (match v
+				[(conzV l r)
+					(define leftResult (aux-generate-substs x l))
+					(define rightResult (aux-generate-substs y r))
+					(if (failure? leftResult)
+						leftResult
+						(if (failure? rightResult)
+							rightResult
+							(append leftResult rightResult)
+						)
+					)
+				]
+				[else (failure "MatchError: expected a cons constructor")]
+			)]
+		)
+	)
+
+	(define lista (aux-generate-substs p v))
+	(if (failure? lista)
+		lista
+		(success lista)
+	)
+)
+
 
 ;;------------;;
 ;; P1.h, P2.c ;;
 ;;------------;;
 
 ;; interp : Expr Env -> Value
-(define (interp expr env) '???)
+;; Intérprete del lenguaje
+(define (interp expr env)
+	(match expr
+		[(num n) (numV n)]
+		[(add l r) (num+ (interp l env) (interp r env))]
+		[(nil) (nilV)]
+		[(conz l r) (conzV (interp l env) (interp r env))]
+		[(fun id body) (closureV id body env)]
+		[(id x) (lookup-env x env)]
+		[(app f e)
+			(define substs (generate-substs (match f
+				[(num n) (parse-pattern n)]
+				[(nil) (parse-pattern '(nil))]
+				[(id x) (parse-pattern x)]
+				[(conz l r) (parse-pattern (list 'cons l r))]
+				[(fun p _) p]
+			) (interp e env)))
+			(match substs
+				[(failure fail) (error fail)]
+				[(success s)
+					(def (closureV the-arg the-body closed-env) (interp f env))
+					(def new-env (extend-env* s closed-env))
+					(interp the-body new-env)
+				]
+			)
+		]
+	)
+)
 
 
 ;;----- ;;
@@ -143,7 +248,7 @@ END utility definitions
 
 #|
 En función de lo implementado en la pregunta anterior, argumente porqué es útil que la función
-generate-subst no lance un error (cuando el valor no calza con el patrón) y, en cambio, retorne
+generate-substs no lance un error (cuando el valor no calza con el patrón) y, en cambio, retorne
 un mensaje.
 
 R: ...
--- Tarea_3/test.rkt
+++ Tarea_3/test.rkt
@@ -2,3 +2,63 @@
 (require "T3.rkt")
 
 (print-only-errors #t)
+
+;; P1.b
+(test (parse '1) (num 1))
+(test (parse '(+ 1 -2)) (add (num 1) (num -2)))
+(test (parse '(nil)) (nil))
+(test (parse '(cons 1 2)) (conz (num 1) (num 2)))
+(test (parse '(list 1 2 3)) (conz (num 1) (conz (num 2) (conz (num 3) (nil)))))
+
+;; P1.d
+(test (parse-pattern '1) (numP 1))
+(test (parse-pattern '(nil)) (nilP))
+(test (parse-pattern 'x) (varP 'x))
+(test (parse-pattern '(cons -1 y)) (conzP (numP -1) (varP 'y)))
+(test (parse-pattern '(list 1 x 3)) (conzP (numP 1) (conzP (varP 'x) (conzP (numP 3) (nilP)))))
+
+;; P1.e
+(test (parse '(fun x x)) (fun (varP 'x) (id 'x)))
+(test (parse '(fun (cons x xs) x)) (fun (conzP (varP 'x) (varP 'xs)) (id 'x)))
+(test (parse '(f x)) (app (id 'f) (id 'x)))
+(test (parse '(list 3 x 1)) (conz (num 3) (conz (id 'x) (conz (num 1) (nil)))))
+(test (parse '((y 0) 2)) (app (app (id 'y) (num 0)) (num 2)))
+
+;; P1.g
+(test (generate-substs (numP 3) (numV 3)) (success '()))
+(test (generate-substs (numP 3) (numV 4)) (failure "MatchError: given number does not match pattern"))
+(test (generate-substs (numP -2) (nil)) (failure "MatchError: expected a number"))
+(test (generate-substs (nilP) (nilV)) (success '()))
+(test (generate-substs (nilP) (numV 2)) (failure "MatchError: expected nil"))
+(test (generate-substs (varP 'x) (numV 3)) (success (list (cons 'x (numV 3)))))
+(test(generate-substs (conzP (varP 'x) (varP 'y)) (conzV (numV 3) (numV 4)))
+	(success (list (cons 'x (numV 3)) (cons 'y (numV 4)))))
+(test (generate-substs (conzP (numP 1) (varP 'y)) (conzV (numV 3) (numV 4)))
+	(failure "MatchError: given number does not match pattern"))
+(test (generate-substs (conzP (varP 'x) (varP 'y)) (nil))
+	(failure "MatchError: expected a cons constructor"))
+
+;; P1.h
+(test (interp (parse '1) empty-env) (numV 1))
+(test (interp (parse '(+ 4 -3)) empty-env) (numV 1))
+(test (interp (parse '(nil)) empty-env) (nilV))
+(test (interp (parse '(cons 7 2)) empty-env) (conzV (numV 7) (numV 2)))
+(test (interp (parse '(cons 0 (nil))) empty-env) (conzV (numV 0) (nilV)))
+
+(test (interp (parse 'x) (extend-env 'x (numV 4) empty-env)) (numV 4))
+(test (interp (app (parse '(fun 5 4)) (num 5)) empty-env) (numV 4))
+(test (interp (app (parse '(fun x (+ x 3))) (num 4)) empty-env) (numV 7))
+(test (interp (parse '(fun x (+ x 2))) empty-env) (closureV (varP 'x) (add (id 'x) (num 2)) empty-env))
+
+(define func (parse '(fun (cons 1 y) y)))
+(test (interp func empty-env) (closureV (conzP (numP 1) (varP 'y)) (id 'y) empty-env))
+(test (interp (app func (conz (num 1) (num 2))) empty-env) (numV 2))
+
+(test/exn (interp (app (parse '(fun 3 1)) (num 1)) empty-env)
+	"MatchError: given number does not match pattern")
+(test/exn (interp (parse '(id x)) empty-env) "LookupError: variable ~a not found")
+(test/exn (interp (add (num -2) (nil)) empty-env) "TypeError: expected a number")
+(test/exn (interp (parse '(0 1)) empty-env) "MatchError: given number does not match pattern")
+(test/exn (interp (app (num 4) (nil)) empty-env) "MatchError: expected a number")
+(test/exn (interp (app (nil) (num 2)) empty-env) "MatchError: expected nil")
+(test/exn (interp (app func (num 3)) empty-env) "MatchError: expected a cons constructor")
